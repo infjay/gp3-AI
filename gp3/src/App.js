889135e0ead1d4acf57fd78f3e48171996ca5ae8
infjay <jawid.nm@gmail.com>
2022-12-05 04:07:37 +0100

    Main Structure Implemented

---
+++ gp3/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Footer , Blog, Possibility , WhatGPT3, Header, Features } from './containers';
+import { CTA, Brand, Navbar } from './components';
+import './App.css'
+const App = () => {
+  return (
+    <div className='App'>
+        <div className='gradient__bg'>
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+    </div>
+  )
+}
+
+export default App
